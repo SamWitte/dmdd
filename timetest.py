5737b39a8822692b81829ec0b0b245dd761bacd0
Vera Gluscevic <verag@ias.edu>
2016-04-25 15:57:37 -0400

    small fixes and changes to timetest; added end and start time kwargs to loglikelihood_time; all compiles for VG

--- timetest.py
+++ timetest.py
@@ -2,13 +2,17 @@
 import numpy as np
 import time
 import dmdd
-def timetest(witht=True,n=100):
+def timetest(witht=True):
     qs = np.linspace(2,10,10)
     tim = np.linspace(0,1.,10)
     if witht:
+        n = 1
+    else:
+        n = 100
+    if witht:
         startt = time.time()
         for i in xrange(n):
-            res = dmdd.rate_UV.loglikelihood_time(qs,tim,dmdd.eff.efficiency_unit,sigma_si=100)
+            res = dmdd.rate_UV.loglikelihood_time(qs,0.,1.,tim,dmdd.eff.efficiency_unit,sigma_si=100)
         endt = time.time()
     else:
         startt = time.time()
