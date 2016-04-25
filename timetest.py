511b200c2a901efafd544be540ea5317d5df4eab
Vera Gluscevic <vera.gluscevic@gmail.com>
2016-04-25 11:29:08 -0400

    small changes to rate_UV and path handling change in dmdd.py

---
+++ timetest.py
@@ -0,0 +1,21 @@
+#!/usr/local/bin/python
+import numpy as np
+import time
+import dmdd
+def timetest(witht=True,n=100):
+    qs = np.linspace(2,10,10)
+    tim = np.linspace(0,1.,10)
+    if witht:
+        startt = time.time()
+        for i in xrange(n):
+            res = dmdd.rate_UV.loglikelihood_time(qs,tim,dmdd.eff.efficiency_unit,sigma_si=100)
+        endt = time.time()
+    else:
+        startt = time.time()
+        for i in xrange(n):
+            res = dmdd.rate_UV.loglikelihood(qs,dmdd.eff.efficiency_unit,sigma_si=100)
+        endt = time.time()
+    print('{}\n'.format((endt-startt)/n))
+
+timetest()
+timetest(witht=False)
