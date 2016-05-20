675f4f37c17d5d82835fd2dd60f5ce2a838a9fe5
Samuel Witte <witte.sam@gmail.com>
2016-05-20 16:32:33 -0700

    label fix

--- PlotMaker.py
+++ PlotMaker.py
@@ -1,10 +1,10 @@
 import UVplots
 from UVplots import *
 
-line_plots_1vsall(nsim=50, startsim=1, masses=[500.],
-                      experiment_names=['Xe'],
-                      simmodels=[SI_Higgs], models=[SI_Higgs, anapole], time_info='Both',
-                      filelabel='Xe_Time_vs_NoTime', allverbose=True,
+line_plots_1vsall(nsim=1, startsim=1, masses=[50.],
+                      experiment_names=['Ge','Xe'],
+                      simmodels=[anapole], models=[SI_Higgs, anapole], time_info='Both',
+                      GF=True, filelabel='TEST', allverbose=True,
                       verbose=True,
                       results_root='/Users/SamWitte/Desktop/dmdd/Storage/results_uv/',
                       saveplots=True, alpha=0.3, xoffset=0.1, fs=20, fs2=18, sigma_lim_file=None,
