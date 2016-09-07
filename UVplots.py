1ebd2c6df40ce75d718e02b1164bf56def57e822
Samuel Witte <witte.sam@gmail.com>
2016-09-07 12:03:57 -0500

    label changes

--- UVplots.py
+++ UVplots.py
@@ -780,9 +780,10 @@ def OneDhistogram(nsim=50, startsim=1, masses=[50.],
                         leg_top[i] = 0.7 * ymax
                         axarr[ax_x, ax_y].plot(100. * xlinspace, probdistr, linewidth=2, color=colors_list[ii])
                         lab[i] += ('\n (Solid) Time: ' + r'[Success: {:.0f}$\%$]'.format(success))
-                    axarr[ax_x, ax_y].text(5, leg_top[i], lab[i], color=colors_list[i], fontsize=10)
-                    axarr[ax_x, ax_y].text(5, 0.85 * ymax, experiment_labels[2 * ii], color='k', fontsize=16)
-                    axarr[ax_x, ax_y].set_ylim([0., ymax])
+
+                        axarr[ax_x, ax_y].text(5, leg_top[i], lab[i], color=colors_list[i], fontsize=10)
+                        axarr[ax_x, ax_y].text(5, 0.85 * ymax, experiment_labels[2 * ii], color='k', fontsize=16)
+                        axarr[ax_x, ax_y].set_ylim([0., ymax])
 
             #axarr[1, 1].text(10, 0.5 * ymax, 'Solid: Time \n Dashed: No Time', color='k', fontsize=10)
             pl.suptitle('True model: {} (mass: {:.0f} GeV)'.format(MODELNAME_TEX[m.name], mass), fontsize=fs)
