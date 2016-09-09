2a1e41b8c5f6c5e7c31065129e623599637d2dea
Samuel Witte <witte.sam@gmail.com>
2016-09-09 13:11:53 -0500

    added multinest params file

---
+++ multinest_parameters.py
@@ -0,0 +1,34 @@
+prior_ranges = {
+    'fnfp_si':(-5,5),
+    'fnfp_f2':(-100,100),
+    'mass':(1,3000),
+    'sigma_si':(0.001, 6000),
+    'sigma_sd':(0.001,100000),
+    'sigma_anapole':(0.001, 6000),
+    'sigma_magdip':(0.001,100000),
+    'sigma_elecdip':(0.001,100000),
+    'sigma_LS':(0.001,100000),
+    'sigma_f1':(0.001,100000),
+    'sigma_f2':(0.001,100000),
+    'sigma_f3':(0.001,100000),
+    'sigma_si_massless':(0.001,100000),
+    'sigma_sd_massless':(0.001,100000),
+    'sigma_anapole_massless':(0.001,100000),
+    'sigma_magdip_massless':(0.001,100000),
+    'sigma_elecdip_massless':(0.001,100000),
+    'sigma_LS_massless':(0.001,100000),
+    'sigma_f1_massless':(0.001,100000),
+    'sigma_f2_massless':(0.001,100000),
+    'sigma_f3_massless':(0.001,100000),
+    'h1':(0.001,1000),
+    'h2':(0.001,1000),
+    'l1':(0.001,1000),
+    'l2':(0.001,1000), 
+    }
+
+
+
+n_live_points = 2000
+evidence_tolerance = 0.001
+sampling_efficiency = 0.3
+basename = '1-'
